ae5f5ba7cb6cbef2b13cbcab5345dea00084e34b
Marko Permanto <marko.permanto@gmail.com>
2019-03-18 23:26:58 +0100

    working on mine

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+	public MeshRenderer blinkRenderer;
+
+	public MineStateDetect detectState;
+	public MineStateIdle idleState;
+	public MineStateAttack attackState;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		detectState.blinkMat = blinkRenderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MineStates.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineState {
+	public virtual void Update() {}
+	public virtual void Enter() { }
+}
+
+public class MineStateIdle : MineState {
+	public Vector3 homePos;
+	public override void Update() {
+		// look for player, otherwise move home
+	}
+}
+
+public class MineStateAttack : MineState {
+	public override void Update() {
+		// do nothing
+	}
+}
+[System.Serializable]
+public class MineStateDetect : MineState {
+	[System.NonSerialized]
+	public Material blinkMat;
+	
+	public Color onColor;
+	public Color offColor;
+	public AnimationCurve blinkCurve;
+
+	float detectTime;
+
+	public override void Enter() {
+		detectTime = Time.time;
+	}
+	public override void Update() {
+		var time = Time.time - detectTime;
+		var blinkyness = blinkCurve.Evaluate(time);
+		blinkMat.SetColor("_EmissionColor", Color.Lerp(offColor, onColor, blinkyness));
+		if (blinkCurve.length <= time) {
+			// start chasing?
+		}
+	}
+}
